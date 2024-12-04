0b7ee332fe0977a9a5b43c0b34b7d1b8ec109283
noahdrakes <noah.i.drakes@gmail.com>
2024-12-04 14:08:44 -0500

    linked encoder update to IRQ pin, switch vibration motor based on encoder side

--- vib_motor_fs_interface_test/vib_motor_fs_interface_test.ino
+++ vib_motor_fs_interface_test/vib_motor_fs_interface_test.ino
@@ -1,23 +1,34 @@
 //#include <VibrationMotor.h>/
+#include <string.h>
 
-const int vibMotorPin1 = 6;
-const int vibMotorPin2 = 5;
+volatile int vibMotorPin1 = 6;
+volatile int vibMotorPin2 = 5;
 
-const int encA = 8;
-const int encB = 9;
+const uint8_t encA = 2;
+const uint8_t encB = 3;
+
+volatile int encoderCount = 0;
+volatile int aState;
+volatile int aLastState; 
+
+// false = [0 to 14]
+// true = [15 to 29]
+volatile uint8_t encoder_side = 0;
+volatile uint8_t last_encoder_side = 0;
+
+enum ENCODER_SIDE{
+  ENCODER_FIRST_HALF,
+  ENCODER_SECOND_HALF
+};
 
 
 int fsrPin1 = 0;
 int fsrPin2 = 1;
 int fsrReading1;
-int fsrReading2;
-
-int counter = 0; 
-int aState;
-int aLastState;  
+int fsrReading2; 
 
 // VibrationMotor myVibrationMotor(motorPin);
-
+using namespace std;
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(115200);
@@ -30,40 +41,97 @@ void setup() {
 
   aLastState = digitalRead(encA);   
 
-  
+  attachInterrupt(digitalPinToInterrupt(encA),updateState, CHANGE );
 
+  clearSerialMon();
+
+  calibrateEncoder();
 }
 
-void loop() {
+void clearSerialMon(){
+  for (int i = 0; i < 5; i++){
+    Serial.println(" ");
+  }
 
+  Serial.println("=============================");
+}
+
+void calibrateEncoder(){
+
+  while(1){
+    Serial.println("Catheter Encoder Calibration !");
+    Serial.println("Twist catheter so that each forse sensor is pointing in the right and left direction");
+    Serial.println("Type 'y' and Press [ENTER] in the Serial Monitor when finished");
+
+    while(Serial.available() <= 0 ){}
+
+    char string = Serial.read();
+    if(string == 'y'){
+      Serial.println("Encoder Calibration Finished !");
+      break;
+    } else {
+      Serial.println("Invalid character. Try again");
+    }
+    encoderCount = 0;
+  }
+    
+}
+
+void updateEncoderPos(){
   aState = digitalRead(encA); // Reads the "current" state of the outputA
    // If the previous and the current state of the outputA are different, that means a Pulse has occured
    if (aState != aLastState){     
      // If the outputB state is different to the outputA state, that means the encoder is rotating clockwise
      if (digitalRead(encB) != aState) { 
-       counter ++;
+       encoderCount++;
      } else {
-       counter --;
+       encoderCount--;
      }
-     Serial.print("Position: ");
-     Serial.println(counter);
+
+     if (encoderCount == -1){
+      encoderCount = 29;
+     } else if (encoderCount == 30){
+      encoderCount = 0;
+     }
+    
+     Serial.print("ENCODER POS: ");
+     Serial.println(encoderCount);
    } 
    aLastState = aState; // Updates the previous state of the outputA with the current state
 
 
-  
-  // put your main code here, to run repeatedly:
+}
+
+// switch vib motor based on catheter encoder position
+void updateVibMotors(){
+  if (encoderCount >=0 && encoderCount <15){
+    encoder_side = ENCODER_FIRST_HALF;
+  } else {
+    encoder_side = ENCODER_SECOND_HALF;
+  }
+
+  if (encoder_side != last_encoder_side){
+    uint8_t temp = vibMotorPin1;
+    vibMotorPin1 = vibMotorPin2; 
+    vibMotorPin2 = temp;
+    Serial.println("here");
+  } 
+
+  last_encoder_side = encoder_side;
+}
+
+
+void updateState(){
+  updateEncoderPos();
+  updateVibMotors();
+}
+
+void loop() {
+
+
+
   fsrReading1 = analogRead(fsrPin1);
   fsrReading2 = analogRead(fsrPin2);
-
-  
-
-  // Serial.println(force);
- 
-
-  // Serial.print("lock in ");
-
-  
 
   fsrReading1 = map(fsrReading1, 0, 850, 0, 1023);
   fsrReading2 = map(fsrReading2, 0, 850, 0, 1023);
